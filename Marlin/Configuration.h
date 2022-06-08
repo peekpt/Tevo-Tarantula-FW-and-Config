8432f0b721e55d9700969d851084218bc217bd27
Paulo Bruckmann <peekpt@gmail.com>
2022-06-08 19:37:04 +0100

    Fixed Extruder Fan pin (7)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2336,17 +2336,17 @@
 // Preheat Constants - Up to 5 are supported without changes
 //
 #define PREHEAT_1_LABEL "PLA"
-#define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED 70
+#define PREHEAT_1_TEMP_HOTEND 200
+#define PREHEAT_1_TEMP_BED 60
 #define PREHEAT_1_FAN_SPEED 0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED 110
+#define PREHEAT_2_TEMP_BED 100
 #define PREHEAT_2_FAN_SPEED 0 // Value from 0 to 255
 
 #define PREHEAT_3_LABEL "PETG"
-#define PREHEAT_3_TEMP_HOTEND 230
+#define PREHEAT_3_TEMP_HOTEND 235
 #define PREHEAT_3_TEMP_BED 80
 #define PREHEAT_3_FAN_SPEED 0 // Value from 0 to 255
 
